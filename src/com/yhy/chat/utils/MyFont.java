d990c4394fdeed52424cc616c8cfaa2f3bce6110
Young <2475064517@qq.com>
2022-05-14 22:00:34 +0800

    工具类

---
+++ src/com/yhy/chat/utils/MyFont.java
@@ -0,0 +1,13 @@
+package com.yhy.chat.utils;
+
+import java.awt.*;
+/**
+ * @author: 杨海勇
+ **/
+public class MyFont {
+	public static Font getFont() {
+		Font f=new Font("宋体",Font.BOLD,16);//根据指bai定字体名du称、样式和磅值大小，创建一个新zhi Font。
+		return f;
+	}
+
+}
---
+++ src/com/yhy/chat/utils/PropertiesUtil.java
@@ -0,0 +1,34 @@
+package com.yhy.chat.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+/**
+ * @author: Ñîº£ÓÂ
+ **/
+public class PropertiesUtil {
+	public static Map<String, String> getPropertiesMap(String path) {
+		Properties p = new Properties();
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			p.load(ClassLoader.getSystemResourceAsStream(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			try {
+				p.load(new FileInputStream(path));
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		for (Object key : p.keySet()) {
+			map.put(key.toString(), p.getProperty(key.toString()));
+		}
+		return map;
+	}
+}
